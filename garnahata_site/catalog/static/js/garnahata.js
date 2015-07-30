1f3c2637f9987040d2388f0ad5327f5e3c7af066
Dmitry Chaplinsky <dchaplinsky@conversionscience.co.uk>
2015-07-30 16:14:04 +0200

    Various fixes for details page, translations, silly JS to build properties table replaced with template logic, etc

--- garnahata_site/catalog/static/js/garnahata.js
+++ garnahata_site/catalog/static/js/garnahata.js
@@ -116,46 +116,4 @@
         detail.map.addLayer(markers);
         detail.map.fitBounds(markers)
     });    
-    
-    $(document).ready(function () {
-
-        $('.objectAdressTable').each(function () {
-            var Column_number_to_Merge = 1;
- 
-            // Previous_TD holds the first instance of same td. Initially first TD=null.
-            var Previous_TD = null;
-            var i = 1;
-            $("tbody",this).find('tr').each(function () {
-                // find the correct td of the correct column
-                // we are considering the table column 1, You can apply on any table column
-                var Current_td = $(this).find('td:nth-child(' + Column_number_to_Merge + ')');
-                 
-                if (Previous_TD == null) {
-                    // for first row
-                    Previous_TD = Current_td;
-                    i = 1;
-                } 
-                else if (Current_td.text() == Previous_TD.text()) {
-                    // the current td is identical to the previous row td
-                    // remove the current td
-                    Current_td.remove();
-                    // increment the rowspan attribute of the first row td instance
-                    Previous_TD.attr('rowspan', i + 1);
-                    i = i + 1;
-                } 
-                else {
-                    // means new value found in current td. So initialize counter variable i
-                    Previous_TD = Current_td;
-                    i = 1;
-                }
-            });
-              
-              $('.ownershipAsset').each(function () {
-                    if($(this).text().length === 0) {
-                        $(this).parent("td").addClass("emptyTd");
-                    }
-              });
-        });
-    });
-    
 })(jQuery); // End of use strict
