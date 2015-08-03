d855f5c2df4495a459caad1554440ab00c3768b7
Dmitry Chaplinsky <dchaplinsky@conversionscience.co.uk>
2015-08-03 15:47:08 +0200

    OSM restored for admin

--- garnahata_site/catalog/static/js/geocoder.init.js
+++ garnahata_site/catalog/static/js/geocoder.init.js
@@ -4,4 +4,9 @@ django.jQuery(window).on('map:init', function (e) {
         geocoder = L.Control.geocoder({
             "position": "topleft",
         }).addTo(detail.map);
+
+    detail.map.addLayer(
+        new L.TileLayer(
+            'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+            {attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'}));
 });
