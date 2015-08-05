4befb2f1d8d377dffca5f1b47ccb2d4ba0e51edd
Dmitry Chaplinsky <dchaplinsky@conversionscience.co.uk>
2015-08-05 22:33:30 +0200

    Fixing rest of issues

--- garnahata_site/garnahata_site/sitemaps.py
+++ garnahata_site/garnahata_site/sitemaps.py
@@ -3,28 +3,25 @@ from django.core.urlresolvers import reverse
 
 
 from catalog.models import Address
-from cms_pages.models import NewsPage, HomePage, StaticPage
+from cms_pages.models import NewsPage, StaticPage, RawHTMLPage
 
 
 class MainXML(sitemaps.Sitemap):
-    priority = 0.5
     changefreq = 'daily'
 
     def items(self):
-        pages = (
+        pages = [
             ('wagtail_serve', ['']),
             ('latest_addresses', ''),
             ('addresses_by_city', ''),
-        )
-        return [pages[0],pages[1],pages[2]]
+        ]
+        return pages
 
     def location(self, item):
         return reverse(item[0], args=item[1])
 
 
 class NewsXML(sitemaps.Sitemap):
-    changefreq = "daily"
-
     def items(self):
         return NewsPage.objects.live()
 
@@ -36,8 +33,6 @@ class NewsXML(sitemaps.Sitemap):
 
 
 class AdressXML(sitemaps.Sitemap):
-    changefreq = "daily"
-
     def items(self):
         return Address.objects.all()
 
@@ -49,10 +44,9 @@ class AdressXML(sitemaps.Sitemap):
 
 
 class StaticXML(sitemaps.Sitemap):
-    changefreq = "daily"
-
     def items(self):
-        return StaticPage.objects.live()
+        return (list(StaticPage.objects.live()) +
+                list(RawHTMLPage.objects.live()))
 
     def location(self, item):
         return item.url
