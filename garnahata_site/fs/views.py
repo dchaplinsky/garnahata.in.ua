2d2b81f5ee593d4fa7ea68dc6553a636dced4cac
Dmitry Chaplinsky <dchaplinsky@conversionscience.co.uk>
2015-08-09 01:34:53 +0200

    Converter from Andrew integrated

--- garnahata_site/fs/views.py
+++ garnahata_site/fs/views.py
@@ -1,26 +1,28 @@
 import os
+from operator import itemgetter
 
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.conf import settings
 from django.contrib.admin.views.decorators import staff_member_required
 
-from .totxt import convert_many
+from translitua.translit import translitua
+
+from fs.totxt import convert_many
 
 
 @staff_member_required
 def index(request):
     links = []
     for root, dirs, files in os.walk(settings.PDF_STORAGE, topdown=True):
-        for filename in files:
-            if filename.endswith('pdf'):
-                links.append(os.path.join(root, filename).split('/files/')[1])
+        if not dirs and files:
+            links.append([root, len(files)])
 
     return render(
         request,
         "fs/index.html",
         {
-            'content': sorted(links),
+            'content': sorted(links, key=itemgetter(0)),
         }
 
     )
@@ -28,15 +30,16 @@ def index(request):
 
 @staff_member_required
 def get_xls(request, path):
-    pdf = os.path.join(settings.PDF_STORAGE, path)
-    if os.path.exists(pdf):
-        res = convert_many(pdf)
+    pdf_dir = os.path.abspath(os.path.join(settings.PDF_STORAGE, path))
+
+    if os.path.exists(pdf_dir) and pdf_dir.startswith(settings.PDF_STORAGE):
+        res = convert_many(pdf_dir + "/*.pdf")
         if res:
             response = HttpResponse(content=res.read(),
                                     content_type='application/ms-excel')
 
             response['Content-Disposition'] = 'attachment; filename=%s.xlsx' \
-                % pdf.rsplit('/')[-1].split('.')[0]
+                % translitua(pdf_dir.rsplit('/')[-1]).replace(" ", "_")
             return response
         else:
             return HttpResponse('Error: File was not returned.')
